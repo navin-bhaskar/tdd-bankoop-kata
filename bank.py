31457b4c88aeea1e8e947c89f71e3e46725aa092
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-26 16:58:45 +0530

    Added add_account() in the test case

--- bank.py
+++ bank.py
@@ -1,5 +1,5 @@
 
 
 class Bank:
-    def __init__(self, bank_name):
+    def __init__(self, bank_name: str):
         self._name = bank_name
--- test_bank.py
+++ test_bank.py
@@ -6,5 +6,6 @@ from bank import Bank
 class TestBan(TestCase):
     def test__add_account_to_bank__bank_acount_added(self):
         new_bank = Bank('Laxmi Chit Fund')
+        new_bank.add_account('1234')
         
 
