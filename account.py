1a10a8f526d8dfc29c64f76cd9643e679888e6bd
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-26 17:01:42 +0530

    Added account class

---
+++ account.py
@@ -0,0 +1,6 @@
+class Account:
+
+    def __init__(self):
+        self.balance = 0
+        # Future use
+        self.history = []
--- bank.py
+++ bank.py
@@ -1,5 +1,10 @@
-
+from account import Account
 
 class Bank:
     def __init__(self, bank_name: str):
         self._name = bank_name
+        self._accounts = dict()
+
+    def add_account(self, account_id: str):
+        self._accounts[account_id] = Account()
+        
