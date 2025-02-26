3db1a3b34755d2f5e5b5720f42e194c466c937c6
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-26 17:07:47 +0530

    adding duplicate account UT check

--- test_bank.py
+++ test_bank.py
@@ -4,8 +4,16 @@ from bank import Bank
 
 
 class TestBan(TestCase):
-    def test__add_account_to_bank__bank_acount_added(self):
+    def test__add_account_to_bank__bank_account_added(self):
         new_bank = Bank('Laxmi Chit Fund')
         new_bank.add_account('1234')
+
+    def test__adding_duplicate_account__raises_exception(self):
+        new_bank = Bank('Laxmi Chit Fund')
+        new_bank.add_account('1234')
+        with self.assertRaises(ValueError) as em:
+            new_bank.add_account('1234')
+
+        self.assertEqual(str(em.exception), "Account '1234' already in records") 
         
 
