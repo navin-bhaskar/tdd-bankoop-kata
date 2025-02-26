f6968c71e17931360aba501b865003296df343c4
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-26 16:57:06 +0530

    Added bank instantiate

--- test_bank.py
+++ test_bank.py
@@ -3,5 +3,8 @@ from unittest import TestCase
 from bank import Bank
 
 
-
+class TestBan(TestCase):
+    def test__add_account_to_bank__bank_acount_added(self):
+        new_bank = Bank('Laxmi Chit Fund')
+        
 
